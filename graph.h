d69ace7c95aaa8bbe0349c121b4f4bb22d177fd1
Gregory Presser <gpress2222@gmail.com>
2021-11-06 20:14:13 -0400

    Fixed an issue and messed with friendship

--- graph.h
+++ graph.h
@@ -8,24 +8,28 @@
 #include "./hash.h"
 
 class graph {
-    private: 
-        // hashtable containing all the vertecies and their pointer inside the list 
-        hashTable hashmap; 
+    
     protected:  
         // Class describing the each vertex
         class vertex{
-            public:
+            friend class graph; 
+            private:
                 std::string name {""}; 
-
                 // edge which is a tuple with the first value the pointer to a vertex
                 // the second value is the cost of the edge
                 std::list<std::tuple<vertex*,int>> edges; 
+            public:
                 vertex() = default; 
                 vertex(const std::string& name); 
+                std::list<std::tuple<vertex*,int>>::iterator begin(){ return edges.begin();}
+                std::list<std::tuple<vertex*,int>>::iterator end(){ return edges.end();}
+                std::string getName(){return name;}
         }; 
         // list of vertecies 
         std::list<vertex> verts; 
-        
+    private: 
+        // hashtable containing all the vertecies and their pointer inside the list 
+        hashTable hashmap; 
 
     public:
         graph() = default; 
@@ -40,7 +44,6 @@ class graph {
         int getSize();
         
         // returns a list of strings containing the names of the vertecies
-        std::list<std::string> getNames(); 
 
 
         // Prints the vertecies and its edges
@@ -49,46 +52,19 @@ class graph {
         //          StartVName2: (endVName1,cost1) (endVName2,cost2) ...
         void print(); 
 
+        // returns true if a vertex with the name exists
+        bool contains(const std::string& node); 
 
-    
+        std::list<vertex>::iterator begin() {return verts.begin();}
+        std::list<vertex>::iterator end() {return verts.end();}
+
+
+        class Pair{
+            public:
+                Pair(vertex* self = nullptr, vertex* path = nullptr);
+                vertex* self; 
+                vertex* path; 
+        };
 };
 
-// An extention of the graph class that implements Dijkstra's Algorthim 
-class graphWithDijkstras : public graph {
-    public: 
-        graphWithDijkstras() : graph() {}
-        
-        // Calculates the min distance between the start node (string) and every node in the graph using Dijkstra's Algorthim
-        // Returns a hashTable with the key being the vertex name and the pv a NodeInfo*
-        hashTable Dijkstra(const std::string& start); 
-
-        // A helper class containing all the information crutial to Dijkstra's algorthim
-        class NodeInfo {
-            public:
-                NodeInfo(const std::string& self, const std::string& path, int weight){
-                    this -> self = self;
-                    this -> path = path;
-                    this -> weight = weight; 
-                }
-                std::string self = {""}; 
-                std::string path = {""}; 
-                int weight = {INT_MAX}; 
-        };
-        
-    private:
-
-        // A helper class used to calculate the min distance internally 
-        class DijkstraHelper {
-            public:
-                DijkstraHelper(graph::vertex* self = nullptr,graph::vertex* path = nullptr){
-                    this -> self = self;
-                    this -> path = path;
-                }
-                
-                graph::vertex* self = {nullptr};
-                graph::vertex* path = {nullptr};
-        };
-
-    
-}; 
 #endif
--- useDijkstras.cpp
+++ useDijkstras.cpp
@@ -8,12 +8,88 @@
 
 using namespace std;
 
+class NodeInfo {
+    public:
+        NodeInfo(const std::string& self, const std::string& path, int weight){
+            this -> self = self;
+            this -> path = path;
+            this -> weight = weight; 
+        }
+        std::string self = {""}; 
+        std::string path = {""}; 
+        int weight = {INT_MAX}; 
+};
+
+
+// Returns the start node as a string if the start node is not in the vertex list then retry
+string getStartNode(graph& g){
+    cout << "Enter name of starting vertex: "; 
+    string startNode; 
+    cin >> startNode; 
+
+    if(!g.contains(startNode)) {
+        cout << "Invalid vertex name: " << startNode << ". Please insert a valid vertex name" << "\n"; 
+        return getStartNode(g); 
+    }
+    return startNode;
+
+
+}
+
+
+// Performs Dijkstra's algorithm on the graph starting from start vertex
+// Returns a hashTable of node info objects with the distances and paths associated with each node
+hashTable Dijkstra(graph &g, const string& start){
+    // initialize the heap and hashtable 
+    heap dheap(g.getSize());
+    hashTable ktable(g.getSize()); 
+    for(auto& v: g){
+        graph::Pair* pair = new graph::Pair(&v); 
+        dheap.insert(v.getName(),v.getName() == start ? 0 : INT_MAX,pair);
+    }
+
+    string vName;
+    int dv;
+    graph::Pair * pair; 
+    while(dheap.deleteMin(&vName,&dv,(void**)&pair) != 1){
+        // Add to the known set 
+        NodeInfo* ni = new NodeInfo(vName,pair->path == nullptr ? vName : pair->path->getName(),dv); 
+        ktable.insert(vName,ni); 
+
+        // for every edge v->w
+        for(auto& e: *(pair->self)){
+            int cvw = get<1>(e);
+            auto* w = get<0>(e); 
+            bool found;  
+            int dw = dheap.getKey(w->getName(),&found);
+            if(!found){
+                NodeInfo* ni = (NodeInfo*)ktable.getPointer(w->getName(),&found); 
+                if(!found) {
+                    cerr << "Unreachable\n";
+                    exit(-1); 
+                }
+                dw = ni->weight; 
+            }
+
+            // if new distance is smaller than current distance 
+            if(dv + cvw < dw){
+                dheap.setKey(w->getName(),dv + cvw);
+                graph::Pair* wp = (graph::Pair*)dheap.getPointer(w->getName());
+                wp->path = pair->self;  
+            }
+        }
+        delete pair; 
+    }
+    return ktable;
+}    
+
+
 // Reads the input file and generates a graph (of type graphWithDijkstras) with the given vertecies and edges
 // file content will be a new line sep file of values tokenized by: 
 // {startnode} {endNode} {weight}
-graphWithDijkstras generateGraph(const string& filename){
+graph generateGraph(const string& filename){
     ifstream inputfile(filename); 
-    graphWithDijkstras g; 
+    graph g; 
     if(inputfile.is_open()){
         string line; 
         while(getline(inputfile, line)){
@@ -25,22 +101,8 @@ graphWithDijkstras generateGraph(const string& filename){
             string end;
             int weight; 
 
-            while(getline(ss,word, ' ')){
-                switch(counter){
-                    case 0:
-                        start = word; 
-                        break;
-                    case 1:
-                        end = word;
-                        break;
-                    case 2:
-                        weight = stoi(word);
-                        break;
-                    default:
-                        cerr << "Unreachable \n"; 
-                }
-                counter++; 
-            }
+            ss >> start >> end >> weight;
+
             g.insert(start,end,weight);
             
         }
@@ -56,12 +118,12 @@ graphWithDijkstras generateGraph(const string& filename){
 // kh is a hashtable that represents the results of Dijkstra's algorthim its value is a graphWithDijkstras::NodeInfo*
 string getPath(const string& start, const string& current, hashTable& kh){
     bool found; 
-    graphWithDijkstras::NodeInfo* dh = (graphWithDijkstras::NodeInfo*)kh.getPointer(current,&found); 
+    NodeInfo* ni = (NodeInfo*)kh.getPointer(current,&found); 
     if(!found){
         cerr << "Unreachable \n";
         exit(-1);
     }
-    string pname = dh->path == "" ? start : dh->path;
+    string pname = ni->path == "" ? start : ni->path;
     
     if(current == start) return "[" + start;
     return getPath(start,pname,kh) + ", " + current; 
@@ -75,20 +137,20 @@ string getPath(const string& start, const string& current, hashTable& kh){
 // a hashtable kh which is the hashtable outputed by Dijkstra's algorthim
 void writeToOutput(const string& filename, const string& startNode, graph& g, hashTable& kh){
     ofstream outstream(filename);
-    for(auto& v: g.getNames()){
+    for(auto& v: g){
         bool found; 
-        graphWithDijkstras::NodeInfo* dhp = (graphWithDijkstras::NodeInfo*)kh.getPointer(v,&found); 
+        NodeInfo* ni = (NodeInfo*)kh.getPointer(v.getName(),&found); 
         if(!found){
             cerr << "Unreachable \n";
             exit(-1);
         }
-        int distance = dhp->weight; 
+        int distance = ni->weight; 
         if(distance < 0 || distance == INT_MAX){
-            outstream << v << ": " << "NO PATH" << "\n";
+            outstream << v.getName() << ": " << "NO PATH" << "\n";
             continue;
         }
-        string fs = getPath(startNode,v,kh) + "]"; 
-        outstream << v << ": " << distance << " " << fs << "\n"; 
+        string fs = getPath(startNode,v.getName(),kh) + "]"; 
+        outstream << v.getName() << ": " << distance << " " << fs << "\n"; 
     }
     outstream.close(); 
 }
@@ -99,14 +161,13 @@ int main(){
     cout << "Enter name of graph file: "; 
     string graphFile;
     cin >> graphFile;
-    graphWithDijkstras g = generateGraph(graphFile);
-    cout << "Enter name of starting vertex: "; 
-    string startNode; 
-    cin >> startNode; 
+    graph g = generateGraph(graphFile);
+    string startNode = getStartNode(g); 
 
     // Apply Dijkstra's algorithm
     auto start = chrono::_V2::steady_clock::now();
-    hashTable ktable = g.Dijkstra(startNode); 
+    // hashTable ktable = g.Dijkstra(startNode); 
+    hashTable ktable = Dijkstra(g,startNode);
     auto end = chrono::_V2::steady_clock::now();
     chrono::duration<double> duration = end-start; 
     cout << "Total time (in seconds) to apply Dijkstra's algorithm: " << duration.count() << endl;
